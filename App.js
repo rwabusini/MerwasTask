6889e562927f56c80a884f12ce1c2b91dcd17ee3
rwabusini <rwabusini@gmail.com>
2020-09-21 23:14:05 +0300

    trying to make stackNavigator

--- App.js
+++ App.js
@@ -1,132 +1,23 @@
-// //import 'react-native-gesture-handler';
-// import React from 'react';
-// import { Text, View, TouchableOpacity , StyleSheet} from 'react-native';
-// import { NavigationNativeContainer } from '@react-navigation/native';
-// import { useIsFocused } from "@react-navigation/core";
-// import { createStackNavigator } from "@react-navigation/stack";
-
-// //import { createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-// //import { SafeAreaView } from 'react-native-safe-area-context';
-// // import BestSellers from './src/screens/BestSellers';
-// // import Featured from './src/screens/Featured';
-// // import NewIn from './src/screens/NewIn';
-// //import Animated from 'react-native-reanimated';
-// //import Container from "./src/lib/routes"
+//import 'react-native-gesture-handler';
+import * as React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { useIsFocused } from "@react-navigation/core";
+import { createStackNavigator } from "@react-navigation/stack";
+import Featured from "./src/screens/Featured"; 
+import NewIn from "./src/screens/NewIn"; 
+import BestSellers from "./src/screens/BestSellers"; 
+import { createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'; 
 
 
-// // function Featured1() {
-// //   return (
-// //      <Featured/>
-// //   );
-// // }
-
-
-
-
-// const HomeScreen = ({ navigation }) => {
-//   navigation.setOptions({
-//     headerRight: () => (
-//       <Button
-//         title="Search"
-//         onPress={() => {
-//           //save the changes
-//           navigation.replace("Search");
-//         }}
-//       />
-//     )
-//   });
-
-
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text>HomeScreen</Text>
-//       <Button
-//         title="Go To Cart Screen"
-//         onPress={() => navigation.navigate("Cart")}
-//       />
-//     </View>
-//   );
-// };
-
-// const CartScreen = ({ navigation }) => {
-//   const isFocused = useIsFocused();
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text style={{ color: isFocused ? "green" : "black" }}>
-//         CartScreen
-//       </Text>
-//       <Button title="Go To Home Screen" onPress={() => navigation.goBack()} />
-//     </View>
-//   );
-// };
-
-// const ProductScreen = ({ navigation }) => {
-//   const isFocused = useIsFocused();
-//   return (
-//     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-//       <Text style={{ color: isFocused ? "green" : "black" }}>
-//         ProductScreen
-//       </Text>
-//       <Button title="Go To Home Screen" onPress={() => navigation.goBack()} />
-//     </View>
-//   );
-// };
-
-
-// const Stack = createStackNavigator();
-
-//  function App() {
-//   return (
-//     <NavigationNativeContainer>
-//       <Stack.Navigator>
-//         <Stack.Screen
-//           options={{ title: "My Home Screen" }}
-//           name="Home"
-//           component={HomeScreen}
-//         />
-//         <Stack.Screen name="Cart" component={CartScreen} />
-//         <Stack.Screen name="Product" component={ProductScreen} />
-//       </Stack.Navigator>
-//     </NavigationNativeContainer>
-//   );
-// }
-
-
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center"
-//   }
-// });
-
-// export default App;
-
-
-
-
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import { Text, View, TouchableOpacity , StyleSheet} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import BestSellers from './src/screens/BestSellers';
-import Featured from './src/screens/Featured';
-import NewIn from './src/screens/NewIn';
-
-//import Animated from 'react-native-reanimated';
-import Container from "./src/lib/routes"
-
-function Featured1() {
-  return (
-     <Featured/>
+ 
+function Featured2() {
+  return ( 
+  <Featured/>     
   );
 }
 
+ 
 function NewIn1() {
   return ( 
   <NewIn/>     
@@ -139,29 +30,87 @@ function BestSellers1() {
   );
 }
 
+const HomeScreen = ({ navigation }) => {
+navigation.setOptions({
+  headerRight: () => (
+    <TouchableOpacity onPress={() => navigation.replace("Search")}>
+      <Text>Search</Text>
+    </TouchableOpacity>
+  ),
+});
+
+return (
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text>HomeScreen</Text>
+    <TouchableOpacity onPress={() => navigation.navigate("Cart")}>
+      <Text>"Go To Cart Screen"</Text>
+    </TouchableOpacity>
+  </View>
+);
+};
+
 
 const Tab = createMaterialTopTabNavigator();
 
+const CartScreen = ({ navigation }) => {
+return (
+  
+      
+  <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text style={{}}>CartScreen</Text>
+    <TouchableOpacity onPress={() => navigation.navigate("Product")}>
+      <Text>"Go To Home Screen"</Text>
+    </TouchableOpacity>
+  </View>
+  
+);
+};
+
+
+      // <Tab.Navigator>
+      //   <Tab.Screen name="NEW IN" component={NewIn1} />
+      //   <Tab.Screen name="BEST SELLERS" component={BestSellers1} />
+      // </Tab.Navigator>
+
+
+const ProductScreen = ({ navigation }) => {
+const isFocused = useIsFocused();
+return (
+<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+  <Text style={{ color: isFocused ? "green" : "black" }}>ProductScreen</Text>
+  <TouchableOpacity onPress={() => navigation.goBack()}>
+    <Text>"Go To Home Screen"</Text>
+  </TouchableOpacity>
+</View>
+);
+};
+
+const Stack = createStackNavigator();
+
+
 function App() {
-  return (
-  <>
- <SafeAreaView style={styles.container}>
-    <NavigationContainer style={styles.container}>
-   <Container/>
-    <Tab.Navigator>
-      <Tab.Screen name="FEATURED" component={Featured1} />
-      <Tab.Screen name="NEW IN" component={NewIn1} />
-      <Tab.Screen name="BEST SELLERS" component={BestSellers1} />
-    </Tab.Navigator>
-    </NavigationContainer>
-   </SafeAreaView>
-    </>
-  );
+return (
+  <NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen
+    
+      name="Home"
+      component={HomeScreen}
+      />
+      <Stack.Screen name="Cart" component={CartScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
+      <Stack.Screen name="Featured" component={Featured2} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
